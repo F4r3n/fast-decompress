4ce3bc691c6e122b1cc9687f7fc2c40bcf9e97a7
f4r3n <guillaume_k@hotmail.fr>
2023-04-26 17:10:17 +0200

    remove local test

--- test.mjs
+++
@@ -1,5 +0,0 @@
-import { untar, tar } from './index.js'
- 
-untar("C:\\Users\\guill\\AppData\\Roaming\\Code\\User\\globalStorage\\4d.4d-analyzer\\tool4D.tar.xz",
-"C:\\Users\\guill\\AppData\\Roaming\\Code\\User\\globalStorage\\4d.4d-analyzer\\tool4D_untar"
-)
