ca75bb7cd430245c1ab5408bfa2566faa3a17486
Guillaume Kotulski <guillaume.kotulski@4d.com>
2023-05-12 11:40:37 +0200

    0.0.3

---
+++ test.mjs
@@ -0,0 +1,4 @@
+import { untarLzma } from './index.js'
+ 
+untarLzma("C:\\Users\\gkotulski\\AppData\\Roaming\\Code\\User\\globalStorage\\4d.4d-analyzer\\test.xz", 
+"C:\\Users\\gkotulski\\AppData\\Roaming\\Code\\User\\globalStorage\\4d.4d-analyzer\\test")
