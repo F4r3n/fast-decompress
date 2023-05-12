954ca09bdf5e6157de9ab947aa3dee94b18c1ea0
Guillaume Kotulski <guillaume.kotulski@4d.com>
2023-05-12 13:25:00 +0200

    remove file

--- test.mjs
+++
@@ -1,4 +0,0 @@
-import { untarLzma } from './index.js'
- 
-untarLzma("C:\\Users\\gkotulski\\AppData\\Roaming\\Code\\User\\globalStorage\\4d.4d-analyzer\\test.xz", 
-"C:\\Users\\gkotulski\\AppData\\Roaming\\Code\\User\\globalStorage\\4d.4d-analyzer\\test")
